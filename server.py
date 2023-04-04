fb80f75bc4e4e48a4c33751ef08023bd327bb9cb
Glitch (glitch-hello-website) <none>
2023-04-04 13:17:21 +0000

    📼🐞 Checkpoint ./server.py:53524510/372 ./requirements.txt:53524510/44

--- server.py
+++ server.py
@@ -1,15 +1,8 @@
 from flask import Flask
-import sqlite3
-import numpy as np
-import pandas as pd
-#from sklearn.linear_model import LinearRegression
+app=Flask(__name__)
 
-app = Flask(__name__) 
 
-#@app.route('/') 
-#def landingpage(): 
-   
- # return render_template('base.html')
 
-if __name__ == '__main__': 
+
+if __name__=='__main__':
   app.run()
