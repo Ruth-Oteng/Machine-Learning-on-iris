c5e0d0b64e10aa9d30900183c72c1898ceac84da
Glitch (glitch-hello-website) <none>
2023-04-04 07:08:50 +0000

    😀🛤 Checkpoint ./server.py:53524510/11

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import *
+from flask import Flask
 import sqlite3
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__) 
 
