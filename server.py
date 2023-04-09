fa9b4e1688e270532d2c53c03a2bb217f8afd0be
Glitch (glitch-hello-website) <none>
2023-04-09 11:55:01 +0000

    🏝🦂 Checkpoint ./templates/newindex.html:53524510/902 ./server.py:53524510/728 ./templates/base.html:53524510/8 ./requirements.txt:53524510/1

--- server.py
+++ server.py
@@ -10,6 +10,26 @@ def home():
    
  return render_template('newindex.html')
 
+@app.route('/p', methods=["POST"]) 
+def mainpage(): 
+  selfstudy= eval(request.form.get("selfstudy"))
+  tut=  eval(request.form.get("tut"))
+  # model prediction
+  url='https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/student-pass-fail-data.csv'
+  df=pd.read_csv(url)
+  df1=df.values
+  
+  X=df1[:,0:2]
+  Y= df1[:,2]
+  model=LinearRegression()
+  model.fit(X,Y)
+  var=model.predict([[selfstudy,tut]])
+  result=var[0]*100
+  if result >50:
+    return render_template('newindex.html', data= str(round(result)) + "% implies a high chance of passing" )
+  else:
+    return render_template('newindex.html', data= str(round(result)) + "% implies you might fail" )
+
 @app.route('/ml') 
 def run():
   url='https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/student-pass-fail-data.csv'
