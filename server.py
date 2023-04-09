5cbc771e7ea8b04e572326168e749f1ef6c328d8
Glitch (glitch-hello-website) <none>
2023-04-09 11:31:59 +0000

    🎁🏈 Checkpoint ./server.py:53524510/427 ./requirements.txt:53524510/27

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import *
 import numpy as np
 import pandas as pd
-
+from sklearn.linear_model import LinearRegression
 app=Flask(__name__)
 
 
@@ -9,6 +9,21 @@ app=Flask(__name__)
 def home(): 
    
  return render_template('newindex.html')
+
+@app.route('/ml') 
+def run():
+  url='https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/student-pass-fail-data.csv'
+  df=pd.read_csv(url)
+  df1=df.values
+  
+  X=df1[:,0:2]
+  Y= df1[:,2]
+  model=LinearRegression()
+  model.fit(X,Y)
+  var=model.predict([[7,27]])
+  
+  return "The prdeicted result is " + str(var[0]* 100) + "%"
+
 
 #app.route('/r')
 #def index():
